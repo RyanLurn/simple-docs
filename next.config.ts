f2b2752c5b525cc1fd6a48f371a0d656c09bbc8b
Ryan Luong <lumintriet@gmail.com>
2025-11-07 09:39:04 +0700

    chore(mdx): add plugin to use GitHub-flavored markdown

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
 
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
+  options: {
+    remarkPlugins: ["remark-gfm"],
+  },
 });
 
 // Merge MDX config with Next.js config
