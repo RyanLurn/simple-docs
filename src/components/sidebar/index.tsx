9fe41c745ad98bf5956715732b18b650be3636a5
Ryan Luong <lumintriet@gmail.com>
2025-11-08 09:31:35 +0700

    feat(ui): add links to sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,10 +1,19 @@
 import type { ComponentProps } from "react";
+import { NavLink } from "@/components/nav-link";
 import { cn } from "@/lib/utils";
 
 function Sidebar({ className, ...props }: ComponentProps<"aside">) {
   return (
-    <aside className={cn("h-full w-1/5", className)} {...props}>
-      <h1>Simple Docs</h1>
+    <aside
+      className={cn("h-full w-1/6 p-6 flex flex-col gap-y-2", className)}
+      {...props}
+    >
+      <h2 className="text-lg font-semibold mt-4 mb-2">Getting Started</h2>
+      <NavLink href="/docs/getting-started/features">Features</NavLink>
+      <NavLink href="/docs/getting-started/installation">Installation</NavLink>
+      <h2 className="text-lg font-semibold mt-4 mb-2">Guides</h2>
+      <NavLink href="/docs/guides/add-page">Add a page</NavLink>
+      <NavLink href="/docs/guides/edit-page">Edit a page</NavLink>
     </aside>
   );
 }
